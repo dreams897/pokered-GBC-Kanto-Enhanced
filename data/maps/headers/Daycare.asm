d744dd04df471f895f23b4654cb9a4128b1e3b24
User <dreams897@proton.me>
2023-06-25 12:22:29 -0700

    Added PC to Day Care

--- data/maps/headers/Daycare.asm
+++ data/maps/headers/Daycare.asm
@@ -1,2 +1,2 @@
-	map_header Daycare, DAYCARE, HOUSE, 0
+	map_header Daycare, DAYCARE, REDS_HOUSE_2, $00
 	end_map_header
--- data/maps/objects/Daycare.asm
+++ data/maps/objects/Daycare.asm
@@ -8,8 +8,8 @@ Daycare_Object:
 	def_bg_events
 
 	def_object_events
-	object_event  2,  3, SPRITE_GRANNY, STAY, RIGHT, 1 ; person
-	object_event  5,  3, SPRITE_GENTLEMAN, STAY, LEFT, 2 ; person
+	object_event  2,  4, SPRITE_GRANNY, STAY, RIGHT, 1 ; person
+	object_event  5,  4, SPRITE_GENTLEMAN, STAY, LEFT, 2 ; person
 	object_event  5,  1, SPRITE_DITTO, WALK, ANY_DIR, 3 ; person
 	object_event  0,  4, SPRITE_BULBASAUR, STAY, RIGHT, 4 ; person
 
