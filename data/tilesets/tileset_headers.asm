cfedcbade9dc71077e5863474aba937e0ba9ca4f
User <dreams897@proton.me>
2023-06-28 23:56:28 -0700

    Fix Celadon diner tileset

--- data/tilesets/tileset_headers.asm
+++ data/tilesets/tileset_headers.asm
@@ -33,5 +33,5 @@ Tilesets:
 	tileset Club,        $07,$17, -1,  -1, TILEANIM_NONE
 	tileset Facility,    $12, -1, -1,  -1, TILEANIM_WATER
 	tileset Plateau,      -1, -1, -1, $45, TILEANIM_WATER
-	tileset Casino,      $15,$36, -1,  -1, TILEANIM_NONE
+	tileset Casino,      $12,$13,$15,  -1, TILEANIM_NONE
 	assert_table_length NUM_TILESETS
