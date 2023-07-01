0e5ca434ce145a3c29f6f6f196d94f0e57123f5d
User <dreams897@proton.me>
2023-07-01 16:56:51 -0700

    Improved Mt. Moon super nerd text

--- data/maps/objects/MtMoonB2F.asm
+++ data/maps/objects/MtMoonB2F.asm
@@ -10,11 +10,11 @@ MtMoonB2F_Object:
 	def_bg_events
 
 	def_object_events
-	object_event 11, 16, SPRITE_ROCKET, STAY, DOWN, 1, OPP_ROCKET, 1
-	object_event 15, 22, SPRITE_ROCKET, STAY, DOWN, 2, OPP_ROCKET, 2
-	object_event 29, 11, SPRITE_ROCKET, STAY, UP, 3, OPP_ROCKET, 3
-	object_event 29, 17, SPRITE_ROCKET, STAY, LEFT, 4, OPP_ROCKET, 4
-	object_event 12,  8, SPRITE_SUPER_NERD, STAY, RIGHT, 5, OPP_SUPER_NERD, 2
+	object_event 12,  8, SPRITE_SUPER_NERD, STAY, RIGHT, 1, OPP_SUPER_NERD, 2
+	object_event 11, 16, SPRITE_ROCKET, STAY, DOWN, 2, OPP_ROCKET, 1
+	object_event 15, 22, SPRITE_ROCKET, STAY, DOWN, 3, OPP_ROCKET, 2
+	object_event 29, 11, SPRITE_ROCKET, STAY, UP, 4, OPP_ROCKET, 3
+	object_event 29, 17, SPRITE_ROCKET, STAY, LEFT, 5, OPP_ROCKET, 4
 	object_event 12,  6, SPRITE_FOSSIL, STAY, NONE, 6 ; person
 	object_event 13,  6, SPRITE_FOSSIL, STAY, NONE, 7 ; person
 	object_event 25, 21, SPRITE_POKE_BALL, STAY, NONE, 8, HP_UP
