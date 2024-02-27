a8cfc54dbe889af2ac373d59c7abecee3d1fd185
dreams897 <dreams897@proton.me>
2024-02-26 22:51:32 -0800

    Pewter City improvements

--- home/npc_movement.asm
+++ home/npc_movement.asm
@@ -50,3 +50,18 @@ RunNPCMovementScript::
 EndNPCMovementScript::
 	farjp _EndNPCMovementScript
 	ret
+	
+DebugPressedOrHeldB:: ; dummy except in _DEBUG
+; This is used to skip Trainer battles, the
+; Safari Game step counter, and some NPC scripts.
+IF DEF(_DEBUG)
+	ld a, [wd732]
+	bit BIT_DEBUG_MODE, a
+	ret z
+	ldh a, [hJoyHeld]
+	bit BIT_B_BUTTON, a
+	ret nz
+	ldh a, [hJoyPressed]
+	bit BIT_B_BUTTON, a
+ENDC
+	ret
